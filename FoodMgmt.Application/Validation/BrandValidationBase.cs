f5e87b0b7593f2be7b4dd1bffc7099e9d9262151
Sejal <sejalpatil2315@gmail.com.com>
2025-11-25 14:51:14 +0530

    brand validation

---
+++ FoodMgmt.Application/Validation/BrandValidationBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodMgmt.Application.Validation
+{
+    public class BrandValidationBase
+    {
+    }
+}
